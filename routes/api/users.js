8562eef598b387a3a00be6a423e975f6466324c9
pjaew94 <55684001+pjaew94@users.noreply.github.com>
2020-09-02 15:35:12 -0400

    Auth middleware with login authentication implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
+const User = require("../../models/User");
+
+const { body, validationResult } = require("express-validator");
 
 // @route   Get api/users
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => {
-    res.send('User Route')
-})
+router.post(
+  "/",
+  [
+    body("name", "Please enter your name.").not().isEmpty(),
+    body("email", "Please include a valid email").isEmail(),
+    body(
+      "password",
+      "Please enter a password with more than 6 characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-module.exports = router
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      console.log(payload);
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+      
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,13 +6,18 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) => res.send('API running'))
+// Initialize middleware
+app.use(express.json({ extended: false }))
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/auth', require('./routes/api/auth'))
+
+
+app.get('/', (req, res) => res.send('API running'))
+
 
 const PORT = process.env.PORT || 5000;
 
